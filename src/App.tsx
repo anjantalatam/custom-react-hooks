c9c662546934480013dc9e7c8c5ca641bd9a9c18
Anjan Talatam <tspa2117@gmail.com>
2023-08-03 08:08:24 +0530

    📦 NEW: add tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
 import Examples from "./examples";
 import "./app.scss";
+import { useState } from "react";
+import clsx from "clsx";
+
+const tabs = [
+  {
+    id: "examples",
+    component: <Examples />,
+    title: "Example",
+  },
+];
 
 function App() {
-  return <Examples />;
+  const [selectedTab, setSelectedTab] = useState(tabs[0].id);
+
+  const handleTabClick = (id: string) => {
+    setSelectedTab(id);
+  };
+
+  return (
+    <div className="m-4">
+      <div className="tabs tabs-boxed">
+        {tabs.map((tab) => (
+          <a
+            className={clsx("tab", { "tab-active": tab.id === selectedTab })}
+            key={tab.id}
+            onClick={() => handleTabClick(tab.id)}>
+            {tab.title}
+          </a>
+        ))}
+      </div>
+
+      <div className="m-3">
+        {tabs.filter((tab) => tab.id === selectedTab)[0].component}
+      </div>
+    </div>
+  );
 }
 
 export default App;
